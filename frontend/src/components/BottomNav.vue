7e654de2a49ab81b8d3736e73889906ccc28861e
urusekai <uru.sekai@gmail.com>
2026-02-28 02:00:41 +0900

    feat: 공통 레이아웃 BottomNav, TabMenu 추가

---
+++ frontend/src/components/BottomNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="bottom-nav">
+    <RouterLink class="nav-item" to="/main">
+      <img src="../assets/icons/main.svg" alt="메인" />
+      <img src="../assets/icons/main-active.svg" alt="메인" />
+    </RouterLink>
+
+    <RouterLink class="nav-item" to="/search">
+      <img src="../assets/icons/search.svg" alt="검색" />
+      <img src="../assets/icons/search-active.svg" alt="검색" />
+    </RouterLink>
+
+    <RouterLink class="nav-item" to="/calendar">
+      <img src="../assets/icons/calendar.svg" alt="캘린더" />
+      <img src="../assets/icons/calendar-active.svg" alt="캘린더" />
+    </RouterLink>
+
+    <RouterLink class="nav-item" to="/my-page">
+      <img src="../assets/icons/menu.svg" alt="전체메뉴" />
+      <img src="../assets/icons/menu.svg" alt="전체메뉴" />
+    </RouterLink>
+  </nav>
+</template>
--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
+import BottomNav from '@/components/BottomNav.vue';
+import TabMenu from '@/components/TabMenu.vue';
 
 const route = useRoute();
 const hasTabs = computed(() => route.meta.hasTabs === true);
@@ -9,6 +11,8 @@ const hasMini = computed(() => route.meta.hasMini === true);
 
 <template>
   <div class="app" :class="{ 'has-tabs': hasTabs, 'has-mini': hasMini }">
+    <TabMenu />
     <RouterView />
+    <BottomNav />
   </div>
 </template>
